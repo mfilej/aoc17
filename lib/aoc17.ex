c02983d0abe94a11297f5760e07a3098831886f9
Miha Filej <miha@filej.net>
2017-12-01 09:00:21 +0100

    Fresh mix repo

---
+++ lib/aoc17.ex
@@ -0,0 +1,18 @@
+defmodule Aoc17 do
+  @moduledoc """
+  Documentation for Aoc17.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc17.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc17_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc17Test do
+  use ExUnit.Case
+  doctest Aoc17
+
+  test "greets the world" do
+    assert Aoc17.hello() == :world
+  end
+end
